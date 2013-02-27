3d0e9bcd99712918cda7b672997ac94613fe04b4
haveatry823 <401068081@qq.com>
2013-02-27 20:10:39 +0800

    删除无用文件

--- lua/ai/zhangong-ai.lua
+++
@@ -1,7 +0,0 @@
-sgs.ai_event_callback[sgs.GameStart].zhangong=function(self,player,data)
-	if player:objectName() ~= self.room:getOwner():objectName() then return end
-	for _,p in sgs.qlist(self.room:getAllPlayers()) do
-		self.room:acquireSkill(p,"#zgzhangong1")
-		self.room:acquireSkill(p,"#zgzhangong2")
-	end
-end
